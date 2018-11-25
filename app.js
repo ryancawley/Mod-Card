0e685f4512940c815be69743058a45835686619f
Ryan Cawley <ryan.cawley@outlook.com>
2018-11-25 16:29:01 -0700

    Rebuilding app to push to github

---
+++ app.js
@@ -0,0 +1,11 @@
+const hand1 = [
+  { name: "wizard", level: 1, img: "./images/wizard.jpg" },
+  { name: "elf", level: 2, img: "./images/elf.jpg" },
+  { name: "ogre", level: 3, img: ".images/ogre.jpg" }
+];
+
+const hand2 = [
+  { name: "goblin", level: 1, img: "./images/goblin.jpg" },
+  { name: "witch", level: 2, img: "./images/witch.jpg" },
+  { name: "dragon", level: 3, img: "./images/dragon.jpg" }
+];
