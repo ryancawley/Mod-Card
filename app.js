12b8dff07068a943be40e6e5f13cceaca2e72a19
Ryan Cawley <ryan.cawley@outlook.com>
2018-11-27 09:52:08 -0700

    added attack buttons, improved function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // update variables to include attack and defense attributes
-let button = document.getElementById("button");
+let drawButton = document.getElementById("drawButton");
 let message = document.getElementById("message");
 let card1Name = document.getElementById("card1Name");
 let card1PhysicalAttack = document.getElementById("card1PhysicalAttack");
@@ -16,7 +16,7 @@ let card2Image = document.getElementById("card2Image");
 
 const goodCard = [
   {
-    Name: "Barding",
+    name: "Barding",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -24,7 +24,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Beorning",
+    name: "Beorning",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -32,7 +32,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Dunedain",
+    name: "Dunedain",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -40,7 +40,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Dwarf",
+    name: "Dwarf",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -48,7 +48,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Eagle",
+    name: "Eagle",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -56,7 +56,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Ent",
+    name: "Ent",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -64,7 +64,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Gondorian",
+    name: "Gondorian",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -72,7 +72,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Grey-Elf",
+    name: "Grey-Elf",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -80,7 +80,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "High-Elf",
+    name: "High-Elf",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -88,7 +88,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Hobbit",
+    name: "Hobbit",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -96,7 +96,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Huorn",
+    name: "Huorn",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -104,7 +104,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Numenorean",
+    name: "Numenorean",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -112,7 +112,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Pukel Man",
+    name: "Pukel Man",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -120,7 +120,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Ranger",
+    name: "Ranger",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -128,7 +128,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Raven",
+    name: "Raven",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -136,7 +136,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Rohirrim",
+    name: "Rohirrim",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -144,7 +144,7 @@ const goodCard = [
     img: ""
   },
   {
-    Name: "Wizard",
+    name: "Wizard",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -155,7 +155,7 @@ const goodCard = [
 
 const badCard = [
   {
-    Name: "Attack Troll",
+    name: "Attack Troll",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -163,7 +163,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Balrog",
+    name: "Balrog",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -171,7 +171,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Barrow-Wight",
+    name: "Barrow-Wight",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -179,7 +179,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Black Numenorean",
+    name: "Black Numenorean",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -187,7 +187,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Cave Troll",
+    name: "Cave Troll",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -195,7 +195,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Corsair",
+    name: "Corsair",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -203,7 +203,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Crebain",
+    name: "Crebain",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -211,7 +211,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Dead Man of Dunharrow",
+    name: "Dead Man of Dunharrow",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -219,7 +219,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Dragon",
+    name: "Dragon",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -227,7 +227,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Drummer Troll",
+    name: "Drummer Troll",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -235,7 +235,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Dunlending",
+    name: "Dunlending",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -243,7 +243,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Easterling",
+    name: "Easterling",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -251,7 +251,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Goblin",
+    name: "Goblin",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -259,7 +259,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Haradrim",
+    name: "Haradrim",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -267,7 +267,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Kraken",
+    name: "Kraken",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -275,7 +275,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Mumakil",
+    name: "Mumakil",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -283,7 +283,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Nazgul",
+    name: "Nazgul",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -291,7 +291,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Orc",
+    name: "Orc",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -299,7 +299,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Spider",
+    name: "Spider",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -307,7 +307,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Uruk-Hai",
+    name: "Uruk-Hai",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -315,7 +315,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Warg",
+    name: "Warg",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -323,7 +323,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Warg-Rider",
+    name: "Warg-Rider",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -331,7 +331,7 @@ const badCard = [
     img: ""
   },
   {
-    Name: "Winged Beast",
+    name: "Winged Beast",
     physicalAttack: 1,
     physicalDefense: 1,
     magicalAttack: 1,
@@ -343,7 +343,7 @@ const badCard = [
 message.innerHTML = "Please draw, Player 1";
 
 function draw() {
-  button.disabled = true;
+  drawButton.disabled = true;
   message.innerHTML = "Player 1 is drawing...";
   let card1 = Math.round(Math.random() * 2);
   setTimeout(function() {
@@ -370,21 +370,31 @@ function draw() {
       card2MagicalDefense.innerHTML =
         "Magical Defense: " + badCard[card2].magicalDefense;
       card2Image.src = badCard[card2].img;
-      setTimeout(function() {
+      attack1Button.disabled = false;
+      attack2Button.disabled = true;
+    }, 1000);
+  }, 1000);
+}
+
+function attack1() {
+  attack1Button.disabled = true;
+  attack2Button.disabled = false;
+}
+
+function attack2() {
+  attack2Button.disabled = true;
+  drawButton.disabled = false;
+}
+
+/*       setTimeout(function() {
         if (goodCard[card1].level > badCard[card2].level) {
           alert("Player 1 Wins!");
         } else if (badCard[card2].level > goodCard[card1].level) {
           alert("Player 2 Wins!");
         } else if ((goodCard[card1].level = badCard[card2].level)) {
           alert("It's a Tie!");
-        }
+        }, 1000)
+        }; */
 
-        button.disabled = false;
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}
-
-/* Races: Alphabetical
-
-*/
+// which buttons to disable and when
+// draw button disabled until end of attack round: drawButton.disabled = false;
